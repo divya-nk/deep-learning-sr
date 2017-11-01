5101ef8f129a9713c8cffc608ad94be952e90901
divya-nk <mnavane@clemson.edu>
2017-11-01 09:28:40 -0400

    defined step_gradient

--- lr_gradient_descent.py
+++ lr_gradient_descent.py
@@ -13,6 +13,15 @@ def step_gradient(current_b, current_m, points, learningRate):
 	# gradient descent
 	b_gradient = 0
 	m_gradient = 0
+	N = float(len(points))
+	for i in range(0, len(points)):
+		x = points[i, 0]
+		y = points[i, 1]
+		b_gradient += -(2/N) * (y - ((current_m * x) + current_b))
+		m_gradient += -(2/N) * x * (y - ((current_m * x) + current_b))
+	new_b = current_b - (learningRate * b_gradient)
+	new_m = current_m - (learningRate * m_gradient)
+	return[new_b, new_m]
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
 	b = starting_b
@@ -35,5 +44,5 @@ def run():
 	print(b)
 	print(m)
 
-if__name__ = '__main__':
+if __name__ == '__main__':
 	run()
